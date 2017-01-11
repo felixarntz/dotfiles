222a4836fbbc59a05b1b2089f569738bb8c455ca
Felix Arntz <felix-arntz@t-online.de>
2017-01-11 22:00:28 +0100

    Fix bug in oh my zsh install script.

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,4 @@
 curl -L http://install.ohmyz.sh | sh
 
-cp ~/dotfiles/zsh/Inconsolata\ for\ Powerline.odt /Library/Fonts/
+cp ~/dotfiles/zsh/Inconsolata\ for\ Powerline.otf /Library/Fonts/
 ln -s ~/dotfiles/zsh/flixos90.zsh-theme ~/.oh-my-zsh/themes/flixos90.zsh-theme
