b92557e389bfe8b51565894f2d96d8373950915e
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-02-06 13:46:17 +0100

    Remove default PHPUnit installation.

--- phpunit/install.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -x "$(command -v phpunit)" ]; then
-	cd /tmp/
-	wget https://phar.phpunit.de/phpunit.phar
-	chmod +x phpunit.phar
-	mv phpunit.phar /usr/local/bin/phpunit
-else
-	phpunit --self-upgrade
-fi
