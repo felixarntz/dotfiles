034432ad03f533275405985e627a9e53a2e01aaa
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-02-22 11:24:52 +0100

    Install Redis server and PHP extension with VVV.

--- vvv/provision/provision-post.sh
+++ vvv/provision/provision-post.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
 
-# Only run installations/updates when we have a connection
-#
-# This code comes from the original VVV provisioning script.
+apt_package_custom_list=(
+	redis-server
+	php7.0-redis
+)
+
+not_installed() {
+  dpkg -s "$1" 2>&1 | grep -q 'Version:'
+  if [[ "$?" -eq 0 ]]; then
+    apt-cache policy "$1" | grep 'Installed: (none)'
+    return "$?"
+  else
+    return 0
+  fi
+}
+
+for pkg in "${apt_package_custom_list[@]}"; do
+	if not_installed "${pkg}"; then
+		apt-get -y update
+		apt-get -y install ${pkg}
+		echo " * Installed apt-get package ${pkg}"
+	fi
+done
+
+cp "/srv/config/custom/init/vvv-start-custom.conf" "/etc/init/vvv-start-custom.conf"
+echo " * Copied /srv/config/custom/init/vvv-start-custom.conf to /etc/init/vvv-start-custom.conf"
 
 cp "/srv/config/custom/gitconfig" "/home/vagrant/.gitconfig"
-echo " * Copied /srv/config/gitconfig to /home/vagrant/.gitconfig"
+echo " * Copied /srv/config/custom/gitconfig to /home/vagrant/.gitconfig"
