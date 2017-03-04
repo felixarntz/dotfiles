9e555d46b0a14feca170bfeb17f11b3c7eaa0897
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-03-04 16:39:46 +0100

    Add some Sublime Text plugins and adjust configuration.

--- sublime-text-3/install.sh
+++ sublime-text-3/install.sh
@@ -7,5 +7,13 @@ if [ ! -d /usr/local/bin/subl ]; then
 fi
 
 ln -s ~/dotfiles/sublime-text-3/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-ln -s ~/dotfiles/sublime-text-3/PHP.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/PHP.sublime-settings
 ln -s ~/dotfiles/sublime-text-3/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+ln -s ~/dotfiles/sublime-text-3/SublimeLinter.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/SublimeLinter.sublime-settings
+
+ln -s ~/dotfiles/sublime-text-3/CSS.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/CSS.sublime-settings
+ln -s ~/dotfiles/sublime-text-3/HTML.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/HTML.sublime-settings
+ln -s ~/dotfiles/sublime-text-3/JavaScript.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JavaScript.sublime-settings
+ln -s ~/dotfiles/sublime-text-3/JSON.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/JSON.sublime-settings
+ln -s ~/dotfiles/sublime-text-3/LESS.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/LESS.sublime-settings
+ln -s ~/dotfiles/sublime-text-3/PHP.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/PHP.sublime-settings
+ln -s ~/dotfiles/sublime-text-3/Sass.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Sass.sublime-settings
