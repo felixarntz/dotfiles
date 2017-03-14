c80500df9934d9a1a7f62c99890f439ede069f2d
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-03-14 20:46:53 +0100

    Use https GitHub URLs.

--- vvv/www/api-api-develop/vvv-init.sh
+++ vvv/www/api-api-develop/vvv-init.sh
@@ -5,7 +5,7 @@ REPO_NAMES=`cat ${VVV_CONFIG} | shyaml get-values sites.${SITE_ESCAPED}.custom.r
 for i in "${REPO_NAMES[@]}"
 do :
   if [[ ! -d "${VVV_PATH_TO_SITE}/$i" ]]; then
-    noroot git clone git@github.com:api-api/$i.git ${VVV_PATH_TO_SITE}/$i
+    noroot git clone https://github.com:api-api/$i.git ${VVV_PATH_TO_SITE}/$i
   else
     cd ${VVV_PATH_TO_SITE}/$i
     noroot git pull
