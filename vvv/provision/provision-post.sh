2e3d7c0b271398dff0c2548a1cb6ea25803a69f5
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-03-16 16:04:44 +0000

    Fix bugs in VVV post provisioning script.

--- vvv/provision/provision-post.sh
+++ vvv/provision/provision-post.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 
-APT_PACKAGE_CUSTOM_LIST=( redis-server php7.0-redis )
+not_installed() {
+  dpkg -s "$1" 2>&1 | grep -q 'Version:'
+  if [[ "$?" -eq 0 ]]; then
+    apt-cache policy "$1" | grep 'Installed: (none)'
+    return "$?"
+  else
+    return 0
+  fi
+}
 
-for pkg in "${APT_PACKAGE_CUSTOM_LIST[@]}"; do
-	if not_installed "${pkg}"; then
-		apt-get -y update
-		apt-get -y install ${pkg}
-		echo " * Installed apt-get package ${pkg}"
-	fi
-done
+if not_installed redis-server; then
+	apt-get -y update
+	apt-get -y install redis-server
+	echo " * Installed apt-get package redis-server"
+fi
 
-cp "/srv/config/custom/init/vvv-start-custom.conf" "/etc/init/vvv-start-custom.conf"
-echo " * Copied /srv/config/custom/init/vvv-start-custom.conf to /etc/init/vvv-start-custom.conf"
+if not_installed php7.0-redis; then
+	apt-get -y update
+	apt-get -y install php7.0-redis
+	echo " * Installed apt-get package php7.0-redis"
+fi
 
-cp "/srv/config/custom/gitconfig" "/home/vagrant/.gitconfig"
-echo " * Copied /srv/config/custom/gitconfig to /home/vagrant/.gitconfig"
+cp "/srv/config/init/vvv-start-custom.conf" "/etc/init/vvv-start-custom.conf"
+echo " * Copied /srv/config/init/vvv-start-custom.conf to /etc/init/vvv-start-custom.conf"
+
+cp "/srv/config/gitconfig" "/home/vagrant/.gitconfig"
+echo " * Copied /srv/config/gitconfig to /home/vagrant/.gitconfig"
