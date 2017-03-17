c664ccaa42fed1a936fadb5e2a18f2d1cea835cb
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-03-17 10:11:01 +0000

    Fix bug in api-api-develop provisioner.

--- vvv/www/api-api-develop/vvv-init.sh
+++ vvv/www/api-api-develop/vvv-init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPO_NAMES=`cat ${VVV_CONFIG} | shyaml get-values sites.${SITE_ESCAPED}.custom.repo_names 2> /dev/null`
+REPO_NAMES=(`cat ${VVV_CONFIG} | shyaml get-values sites.${SITE_ESCAPED}.custom.repo_names 2> /dev/null`)
 
 for i in "${REPO_NAMES[@]}"
 do :
