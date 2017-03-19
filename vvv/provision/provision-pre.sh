cf84ac216d8eb3b0517d02f103e64d57507f05c2
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-03-19 01:05:09 +0000

    Add GitHub to known_hosts for vagrant user as well in VVV pre provisioner.

--- vvv/provision/provision-pre.sh
+++ vvv/provision/provision-pre.sh
@@ -5,8 +5,19 @@ if [[ ! -d /root/.ssh ]]; then
 fi
 
 if [[ ! -f /root/.ssh/known_hosts ]]; then
-  echo "Add github.com to known_hosts."
+  echo "Add github.com to root known_hosts."
   touch /root/.ssh/known_hosts && ssh-keyscan -H github.com >> /root/.ssh/known_hosts && chmod 600 /root/.ssh/known_hosts
+else
+  echo "github.com is already added to root known_hosts."
+fi
+
+if [[ ! -d ~/.ssh ]]; then
+  mkdir ~/.ssh
+fi
+
+if [[ ! -f ~/.ssh/known_hosts ]]; then
+  echo "Add github.com to known_hosts."
+  touch ~/.ssh/known_hosts && ssh-keyscan -H github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
 else
   echo "github.com is already added to known_hosts."
 fi
