684e052d9f7d7fb98bfcac62bfa7c7280f2317c2
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-03-20 13:31:24 +0000

    Add jslint to global node modules.

--- node/install.sh
+++ node/install.sh
@@ -2,7 +2,7 @@ NODE_MODULES_DIR="$(npm root -g)"
 readonly NODE_MODULES_DIR
 cd $NODE_MODULES_DIR
 
-NODE_MODULES=( "grunt-cli" "gulp-cli" "bower" "cordova" "ionic" "jshint" )
+NODE_MODULES=( "grunt-cli" "gulp-cli" "bower" "cordova" "ionic" "jshint" "jslint" )
 for i in "${NODE_MODULES[@]}"; do
 	if [ ! -d "$i" ]; then
 		echo "Installing $i..."
