2e816465e4220b8ea2615ace576e042d9d5b6f95
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-12-18 14:39:10 +0100

    Fix broken if-clause in VVV preprovisioner.

--- vvv/provision/provision-pre.sh
+++ vvv/provision/provision-pre.sh
@@ -8,7 +8,7 @@ if [[ ! -f /root/.ssh/known_hosts ]]; then
 	echo "Add github.com to root known_hosts."
 	touch /root/.ssh/known_hosts && ssh-keyscan -H github.com >> /root/.ssh/known_hosts && chmod 600 /root/.ssh/known_hosts
 else
-	if [ -z `ssh-keygen -F github.com` ]; then
+	if [[ -z `ssh-keygen -F github.com` ]]; then
 		ssh-keyscan -H github.com >> /root/.ssh/known_hosts
 	else
 		echo "github.com is already added to root known_hosts."
@@ -23,7 +23,7 @@ if [[ ! -f ~/.ssh/known_hosts ]]; then
 	echo "Add github.com to known_hosts."
 	touch ~/.ssh/known_hosts && ssh-keyscan -H github.com >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts
 else
-	if [ -z `ssh-keygen -F github.com` ]; then
+	if [[ -z `ssh-keygen -F github.com` ]]; then
 		ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 	else
 		echo "github.com is already added to known_hosts."
