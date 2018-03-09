0a171436bdc1fd6ac03c2ae66a5884a75733fdd5
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-03-09 14:27:19 -0500

    Install eslint-react plugin and add global .eslintrc file.

--- node/install.sh
+++ node/install.sh
@@ -2,7 +2,7 @@ NODE_MODULES_DIR="$(npm root -g)"
 readonly NODE_MODULES_DIR
 cd $NODE_MODULES_DIR
 
-NODE_MODULES=( "grunt-cli" "gulp-cli" "bower" "cordova" "ionic" "postcss" "csslint" "stylelint" "jshint" "jslint" "eslint" )
+NODE_MODULES=( "grunt-cli" "gulp-cli" "bower" "cordova" "ionic" "postcss" "csslint" "stylelint" "jshint" "jslint" "eslint" "eslint-plugin-react" )
 for i in "${NODE_MODULES[@]}"; do
 	if [ ! -d "$i" ]; then
 		echo "Installing $i..."
