870ff3ac52cd1b2fd005445f48e7b42dc54605d0
Felix Arntz <felixarntz@google.com>
2018-10-29 17:21:54 +0100

    Update Homebrew packages.

--- php/path.zsh
+++ php/path.zsh
@@ -1,2 +1,2 @@
-export PATH="$(brew --prefix php72)/bin:$PATH"
-export PATH="$(brew --prefix php72)/sbin:$PATH"
+export PATH="$(brew --prefix php)/bin:$PATH"
+export PATH="$(brew --prefix php)/sbin:$PATH"
